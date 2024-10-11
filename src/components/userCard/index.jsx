e0da5762ea33b1661d29798b63d37619d5a55d9a
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2024-10-11 13:27:20 +0200

    feat: random user card from public api

--- src/components/userCard/index.jsx
+++ src/components/userCard/index.jsx
@@ -1,58 +1,84 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "../../redux/slices/userSlice";
-import { Card, Avatar, Typography, Row, Col } from "antd";
+import { Card, Avatar, Typography, Row, Col, Button, Flex } from "antd";
 const { Title, Text } = Typography;
 
 export default function UserCard() {
   const { data, status, error } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  useEffect(() => {
+
+  function handleClick() {
     dispatch(fetchUser());
-    if (status === "succeeded") {
-      console.log(data.results[0]);
+  }
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchUser());
     }
-  }, []);
-  return (
-    <></>
-    // <Card
-    //   hoverable
-    //   style={{ width: 300, margin: "16px auto" }}
-    //   cover={
-    //     <Avatar
-    //       src={user.image}
-    //       size={128}
-    //       style={{ margin: "16px auto", display: "block" }}
-    //     />
-    //   }
-    // >
-    //   <Title level={4}>
-    //     {user.title} {user.firstName} {user.lastName}
-    //   </Title>
+  }, [status, dispatch]);
 
-    //   <Row gutter={[8, 8]}>
-    //     <Col span={24}>
-    //       <Text strong>Location:</Text>
-    //       <Text>
-    //         {user.city}, {user.state}, {user.country}
-    //       </Text>
-    //     </Col>
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  if (status === "failed") {
+    return <div>Error: {error}</div>;
+  }
+  if (status === "succeeded" && data) {
+    const user = data.results[0];
 
-    //     <Col span={24}>
-    //       <Text strong>Email:</Text>
-    //       <Text>{user.email}</Text>
-    //     </Col>
+    return (
+      <Flex vertical align="center">
+        <Card
+          hoverable
+          style={{
+            width: 300,
+            margin: "16px auto",
+            background: "linear-gradient(135deg, #f0f0f0 0%, #d6e0f0 100%)",
+            borderRadius: "12px",
+          }}
+          cover={
+            <Avatar
+              src={user.picture.medium}
+              size={128}
+              style={{ margin: "16px auto", display: "block" }}
+            />
+          }
+        >
+          <Title level={4}>
+            {user.name.title} {user.name.first} {user.name.last}
+          </Title>
 
-    //     <Col span={24}>
-    //       <Text strong>Phone:</Text>
-    //       <Text>{user.phone}</Text>
-    //     </Col>
+          <Row gutter={[8, 8]}>
+            <Col span={24}>
+              <Text strong>Location:</Text>
+              <Text>
+                {user.location.city}, {user.location.state},
+                {user.location.country}
+              </Text>
+            </Col>
 
-    //     <Col span={24}>
-    //       <Text strong>Date of Birth:</Text>
-    //       <Text>{user.dob}</Text>
-    //     </Col>
-    //   </Row>
-    // </Card>
-  );
+            <Col span={24}>
+              <Text strong>Email:</Text>
+              <Text>{user.email}</Text>
+            </Col>
+
+            <Col span={24}>
+              <Text strong>Phone:</Text>
+              <Text>{user.phone}</Text>
+            </Col>
+
+            <Col span={24}>
+              <Text strong>Date of Birth:</Text>
+              <Text>{user.dob.date}</Text>
+            </Col>
+          </Row>
+        </Card>
+        <Button type="primary" onClick={handleClick}>
+          Spin
+        </Button>
+      </Flex>
+    );
+  }
+  return null;
 }
